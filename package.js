e34f143692c27e8a1e1014e783243ed173bbe1af
ian40k <groupanimators4@gmail.com>
2025-10-31 19:48:07 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,8 @@
+{
+  "name": "real-3d-racing-game",
+  "version": "1.0.0",
+  "scripts": {
+    "dev": "python3 -m http.server 8000",
+    "build": "echo 'No build needed for static site'"
+  }
+      }
